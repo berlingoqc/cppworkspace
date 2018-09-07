33c4c2d058416fc8eb051397ff1c678dba550f17
William Quintal <williamquintal95@gmail.com>
2018-09-07 15:36:35 -0400

    travail lab3

--- crazycv/src/lab3/colorswitcher.cpp
+++ crazycv/src/lab3/colorswitcher.cpp
@@ -5,9 +5,16 @@
 
 class RGBtoBlackVideo : public PixelVideoModifier {
 	private:
-		cv::Vec3b lower_blue, higher_blue;
+		// Valeur de d'interval pour mes différents couleurs a detecter
+		cv::Vec3b lower_green, higher_green;
 		cv::Vec3b lower_red, higher_red;
 		cv::Vec3b lower_yellow, higher_yellow;
+
+		// Matrix pour detecter les objets une fois l'image convertit
+		uchar** matrix;
+
+		// Nbr de pixel de chaque couleur dominant
+		int nbrRed, nbrYellow, nbrGreen;
 
 		bool Between(Vec3b v, Vec3b top, Vec3b bot) {
 			return v[0] >= bot[0] && v[0] <= top[0] && v[1] >= bot[1] && v[1] <= top[1] && v[2] >= bot[2] && v[2] <= top[2];
@@ -24,11 +31,14 @@ class RGBtoBlackVideo : public PixelVideoModifier {
 			} else {
 				vec = Vec3b(255,255,255);
 			}
-		}	
+		}
 };
+
 
 int main(int argv,char ** argc)
 {
+
+	// AJOUTER un truc pour loader les settings de couleur depuis un fichier yaml
 	RGBtoBlackVideo video;
 	video.Start();
 }
--- cuda/src/lab1/kernel.cu
+++ cuda/src/lab1/kernel.cu
@@ -1,51 +1,81 @@
-#include "../../include/helpme.h"
+#include <stdio.h>
+#include "cuda_runtime.h"
+
+typedef unsigned char uchar;
+
+static void HandleError(cudaError_t err, const char *file, int line) {
+	if (err != cudaSuccess) {
+		printf("%s in %s at line %d\n", cudaGetErrorString(err), file, line);
+		return;
+		//exit(EXIT_FAILURE);
+	}
+}
+
+#define HANDLE_ERROR(err) (HandleError(err,__FILE__.__LINE__))
+
+#define HANDLE_NULL(a) { \
+    if (a == NULL) { printf("Host memory failed in %s at line %d\n", __FILE__,__LINE__);\
+    exit(EXIT_FAILURE);}}
+
+// Round le resultat de a / b a l'int superieur le plus pres
+int iDivUp(int a, int b) {
+	return (a % b != 0) ? (a / b + 1) : (a / b);
+}
 
 
 // Je suis un kerne qui multiplie les elements d'une liste par une scalaire pis qui les mets dans une autre liste
-__global__ static void Kernel_ScalairArray_Int(int *ArrayA, int k, int *ArrayR,int size) {
-    int index =  blockDim.x;
-    if (index < size)
-        ArrayR[index] = ArrayA[index] * k;
+__global__ static void Kernel_ScalairArray_Int(uchar *ArrayA, int k, uchar *ArrayR,int size) {
+    int index =  blockIdx.x * blockDim.x + threadIdx.x;
+    ArrayR[index] = ArrayA[index] * k;
 }
 
-int BLOCK_SIZE = 1;
-int THREAD_COUNT = 0;
+int BLOCK_SIZE = 50;
+
+extern "C" cudaError_t StartKernel_ScalairArray_Int(uchar *pArrayA, int k, uchar *pArrayR, int size) {
+
+	// Calcul le nombre de thread par bloc que j'ai besoin
+    int BLOCK_COUNT = iDivUp(size,BLOCK_SIZE);
+
+    printf("Starting cuda kernel with %d 1D Blocks and %d 1D Threads\r\n",BLOCK_COUNT,BLOCK_SIZE);
 
 
-extern "C" cudaError_t StartKernel_ScalairArray_Int(int *pArrayA, int k, int *pArrayR, int size) {
     // Assure qu'on peut belle et bien utiliser cuda
     cudaError_t cudaStatus = cudaSetDevice(0);
     if(cudaStatus != cudaSuccess) {
         return cudaStatus;
     }
     //Crée nos pointeur utiliser par cuda
-    int *ArrayA, *ArrayR;
+    uchar *ArrayA, *ArrayR;
+
     // Alloue l'espace mémoire des deux ArrayRices sur le gpu
     // calcul de l'espace de notre array de pixel qui représente l'image
-    size_t memSize = size * sizeof(int);
+    size_t memSize = size * sizeof(uchar);
     cudaStatus = cudaMalloc( (void**)&ArrayA,memSize);
-    if(cudaStatus != cudaSuccess){
+    
+	if(cudaStatus != cudaSuccess){
         return cudaStatus;
     }
-    cudaStatus = cudaMalloc( (void**)&ArrayR,memSize);
+    
+	cudaStatus = cudaMalloc( (void**)&ArrayR,memSize);
     if(cudaStatus != cudaSuccess) {
         return cudaStatus;
     }
-    // Copie l'array de donnnée vers le gpu
+    
+	// Copie l'array de donnnée vers le gpu
     cudaStatus = cudaMemcpy(ArrayA,pArrayA, memSize, cudaMemcpyHostToDevice);
     if(cudaStatus != cudaSuccess) {
         return cudaStatus;
     }
 
     // Démarre le kernel
-    Kernel_ScalairArray_Int<<<size,1>>>((int*)ArrayA,(int)k,(int*)ArrayR,(int)size);
+    Kernel_ScalairArray_Int<<<BLOCK_COUNT,BLOCK_SIZE>>>((uchar*)ArrayA,(int)k,(uchar*)ArrayR,(int)size);
 
     if(cudaDeviceSynchronize() == cudaSuccess) {
-        printf("Finit d'execution du kernel");
+        printf("Finit d'execution du kernel\r\n");
     }
 
     // Fait une copie de l'array de resultat du gpu vers le cpu
-    cudaStatus = cudaMemcpy(pArrayR,ArrayR,memSize,cudaMemcpyHostToDevice);
+    cudaStatus = cudaMemcpy(pArrayR,ArrayR,memSize,cudaMemcpyDeviceToHost);
     if(cudaStatus != cudaSuccess) {
         return cudaStatus;
     }
