e2206d8caf8755b63c3d58f2888dbdfffac8bd4e
William Quintal <william95quintalwilliam@outlook.com>
2018-09-17 18:21:24 +0000

    vision lol

--- crazycv/src/lab4/colorswitcher.cpp
+++ crazycv/src/lab4/colorswitcher.cpp
@@ -5,31 +5,30 @@
 
 class RGBtoBlackVideo : public PixelVideoModifier {
 	private:
+		// Valeur de d'interval pour mes différents couleurs a detecter
+		cv::Vec3b lower_green, higher_green;
+		cv::Vec3b lower_red, higher_red;
+		cv::Vec3b lower_yellow, higher_yellow;
+
 		// Matrix pour detecter les objets une fois l'image convertit
 		uchar** matrix;
 
 		// Nbr de pixel de chaque couleur dominant
 		int nbrRed, nbrYellow, nbrGreen;
 
-		bool Between(Vec3b v,int lowerH, int higherH, int lowerV) {
-			return v[0] >= lowerH && v[0] <= higherH && v[2] >= lowerV;
+		bool Between(Vec3b v, Vec3b top, Vec3b bot) {
+			return v[0] >= bot[0] && v[0] <= top[0] && v[1] >= bot[1] && v[1] <= top[1] && v[2] >= bot[2] && v[2] <= top[2];
 		}
 
 		void SetTo(Vec3b *vec,int value) { vec[0]=value; vec[1]=value; vec[2]=value; }
-	public:
-		RGBtoBlackVideo() {
-		}
 
 	protected:
-		void ModifierMat(Mat& img) {
-			cvtColor(img,img,CV_RGB2HSV);
-		}
 		void Modifier(Vec3b * vec) {
-			if(Between(*vec,80,143,50)) {
+			if(Between(*vec,lower_green,higher_green)) {
 				SetTo(vec,0);
-			} else if(Between(*vec,80,143,50)) {
+			} else if(Between(*vec,lower_red,higher_red)) {
 				SetTo(vec,0);
-			} else if(Between(*vec,80,143,50)) {
+			} else if(Between(*vec,lower_yellow,higher_yellow)) {
 				SetTo(vec,0);
 			} else {
 				SetTo(vec,255);
--- include/video.hpp
+++ include/video.hpp
@@ -1,68 +1,68 @@
 #include "cvheaders.hpp"
 
 
-
 class PixelVideoModifier {
-    private:
-        int device = 0;
-        std::string recordLocation;
-
-        void iterateImage(cv::Mat& image) {
-             for(int y = 0; y < image.rows;++y) 
-	            for( int x = 0; x < image.cols;++x)
-	            {
-		            // get le pixel
-					cv:Vec3b* vec = image.ptr<Vec3b>(x,y);
-		            // set le pixel avec le resultat de la fonction passé
-	                Modifier(vec);
-	            }
-        }
+	private:
+		int device = 0;
+		std::string recordLocation;
 
 	protected:
-		// Modifier est la fonction qui est appeler pour modifier mon pixel
+		void iterateFrame(cv::Mat& image);
+		
+		// Mes fonctions virtuels a implémenter
+		virtual void ModifierMat(Mat& mat) {  }
 		virtual void Modifier(cv::Vec3b *v) { }
-		// HandleKey est la fonction qui parse les key entrer par l'usager
-		virtual bool HandleKey(int k) {
-			if(cv::waitKey(0)) {
-				return true;
-			}
+		virtual bool HandleKey(int l) {
+			cv::waitKey();
 			return false;
 		}
 
-    public:
-		PixelVideoModifier() {}
+	public:
+		PixelVideoModifier() { }
 
-        bool Start() {
-            cv::VideoCapture cap(device);
-			
-            if(!cap.isOpened()) {
-		        return false;
-            }
-            cap.set(CAP_PROP_FRAME_HEIGHT,1280);
-            cap.set(CAP_PROP_FRAME_WIDTH,720);
-            string windowName = "Pixel Video Modifier";
-
-        	cv::namedWindow(windowName);
-        	cv::Mat img;
-
-        	while(1) {
-        		if(!cap.read(img)) {
-        			std::cerr << "Error capturing " << std::endl;
-        			return false;
-        		}
-				// change l'image capturer de colorspace pour HSV
-				//cv::cvtColor(img,CV_BGR2HSV);
-				// applique la modification sur l'image
-				iterateImage(img);
-				// l'affiche
-                imshow(windowName,img);
-        		int k = cv::waitKey(0);
-				if(HandleKey(k)) {
-					break;
-				}
-	       }
-           return true;
-        }
-    
+		bool Start();
 };
 
+
+void PixelVideoModifier::iterateFrame(cv::Mat& image) {
+	for(int y = 0; y < image.rows;++y) 
+		for( int x = 0; x < image.cols;++x) {
+		    // get le pixel
+			cv:Vec3b* vec = image.ptr<Vec3b>(x,y);
+		    // set le pixel avec le resultat de la fonction passé
+	        Modifier(vec);
+	 	}
+}
+
+bool PixelVideoModifier::Start() {
+    cv::VideoCapture cap(device);
+
+    if(!cap.isOpened()) {
+		return false;
+    }
+    //cap.set(CAP_PROP_FRAME_HEIGHT,1280);
+    //cap.set(CAP_PROP_FRAME_WIDTH,720);
+    string windowName = "Pixel Video Modifier";
+
+    cv::namedWindow(windowName);
+    cv::Mat img;
+
+    while(1) {
+    	if(!cap.read(img)) {
+    		std::cerr << "Error capturing " << std::endl;
+    		return false;
+    	}
+		// change l'image capturer de colorspace pour HSV
+		ModifierMat(img);
+		// applique la modification sur l'image
+		iterateFrame(img);
+		// l'affiche
+        imshow(windowName,img);
+      	int k = cv::waitKey(30);
+		if(HandleKey(k)) {
+			break;
+		}
+	}
+    return true;
+}
+    
